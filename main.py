ad2465fb7e8a28be94ce9589dca8c7789969efe9
Eric Rustrum <rustrumek@gmail.com>
2020-03-19 10:37:55 -0700

    Created the main file
    :)

---
+++ main.py
@@ -0,0 +1,44 @@
+GOING = True
+import os
+while GOING:
+    os.system('clear')
+    text = input('Please copy+paste the text here: ')
+    splittext = text.split(' ')
+    debug = input('Debug output? [y/n] ') == 'y'
+    sec = 0
+    frst = 0
+    thrd = 0
+    try:
+        splittext = splittext.split('.')
+    except:
+        pass
+
+    for i in range(len(splittext)):
+        if splittext[i].lower() == 'you':
+            sec = sec + 1
+        elif splittext[i].lower() in ['i', 'me', 'we', 'our', 'my']:
+            frst = frst + 1
+        elif splittext[i].lower() in ['he', 'she', 'they']:
+            thrd = thrd + 1
+
+    print('\n')
+    if debug:
+        print('First Person count: ' + str(frst))
+        print('Second Person count: ' + str(sec))
+        print('Third Person count: ' + str(thrd))
+
+    print('\n====[RESULT]====')
+    if thrd > sec:
+        if thrd > frst:
+            print('Third Person.')
+        else:
+            print('First Person.')
+    elif frst < sec:
+        print('Second Person.')
+    elif sec < frst:
+        print('First Person.')
+    else:
+        print('I cannot tell. Try using the debug feature.')
+        
+    GOING = input('\n\nAgain? [y/n] ') == 'y'
+
